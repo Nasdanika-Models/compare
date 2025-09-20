953e3963acab17e17273389725ad1e6c4581250f
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-09-20 09:19:59 -0400

    Maven Central release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# crew-ai
-A model representing CrewAI agents, tools, crew. The model is intended for education, semantic mapping, and code generation
+# EMF Compare
+
+Eclipse [EMF Compare Model](https://github.com/eclipse-emf-compare/emf-compare/tree/master/plugins/org.eclipse.emf.compare) wrapped 
+into a Maven jar/Java module to make it available in the Maven ecosystem on [Maven Central](https://central.sonatype.com/artifact/org.nasdanika.models.compare/model)
--- ecore/src/test/java/org/nasdanika/models/compare/ecore/tests/TestCompareModelDocGen.java
+++ ecore/src/test/java/org/nasdanika/models/compare/ecore/tests/TestCompareModelDocGen.java
@@ -12,7 +12,13 @@ import java.util.function.Function;
 
 import org.eclipse.emf.common.util.DiagnosticException;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.AttributeChange;
+import org.eclipse.emf.compare.CompareFactory;
 import org.eclipse.emf.compare.ComparePackage;
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.scope.DefaultComparisonScope;
+import org.eclipse.emf.compare.scope.IComparisonScope;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.junit.jupiter.api.Test;
@@ -36,6 +42,20 @@ import org.nasdanika.ncore.NcorePackage;
  *
  */
 public class TestCompareModelDocGen {
+
+	// Testing that model classes are exported properly
+	@Test
+	public void testCompare() throws Exception {
+		AttributeChange attrChange = CompareFactory.eINSTANCE.createAttributeChange();
+		attrChange.setAttribute(ComparePackage.Literals.ATTRIBUTE_CHANGE__VALUE);
+		
+		AttributeChange oAttrChange = CompareFactory.eINSTANCE.createAttributeChange();
+		oAttrChange.setAttribute(ComparePackage.Literals.COMPARISON__THREE_WAY);
+		
+		IComparisonScope scope = new DefaultComparisonScope(attrChange, oAttrChange, null);
+		Comparison comparison = EMFCompare.builder().build().compare(scope);
+		comparison.getDifferences().forEach(System.out::println);		
+	}
 	
 	@Test
 	public void testGenerateCompareModelDoc() throws IOException, DiagnosticException {
@@ -106,7 +126,7 @@ public class TestCompareModelDocGen {
 		
 		System.out.println("There are " + errorCount + " site errors");
 		
-		if (errorCount != 100) {
+		if (errorCount != 121) {
 			throw new ExecutionException("There are problems with pages: " + errorCount);
 		}		
 	}
